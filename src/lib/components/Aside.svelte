83bdbfc346d73b8233a6b2b2532e81c8ab9d526f
csae8092 <p.andorfer@gmail.com>
2025-12-19 09:41:28 +0100

    minor things

--- src/lib/components/Aside.svelte
+++ src/lib/components/Aside.svelte
@@ -1,16 +1,18 @@
 <script>
 	import { base } from '$app/paths';
-	
+
 	let { corpusData, currentName } = $props();
 </script>
 
-<aside>
+<div>
 	<h2>Corpora</h2>
 	<ul>
 		{#each corpusData as x}
-			{#if x.name !== currentName}
+			{#if x.name == currentName}
+				<li><a href="{base}/{x.name}"><strong>{x.name}</strong></a></li>
+			{:else}
 				<li><a href="{base}/{x.name}">{x.name}</a></li>
 			{/if}
 		{/each}
 	</ul>
-</aside>
+</div>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,23 +1,28 @@
 <script>
 	import Aside from '$lib/components/Aside.svelte';
+	import { loremIpsum } from 'lorem-ipsum';
 	let { data } = $props();
 	let item = $derived(data.item);
 	let info = $derived(item.name === item.info);
+	const dummyText = $derived(loremIpsum({
+		count: 10,
+		units: 'paragraphs',
+		seed: item.name
+	}));
+	const pageTitle = $derived(
+		item.name === item.info ? item.name : item.info
+	)
 </script>
 
 <svelte:head>
-	<title>{item.name}</title>
-	{#if !info}
-		<meta name="description" content={item.info} />
-	{/if}
+	<title>{pageTitle}</title>
 </svelte:head>
 
-<h1 class="text-center">{item.name}</h1>
-<p class="text-center lead">
-	{#if !info }
-		{item.info}
-	{:else}
-		Keine weitere Information vorhanden
-		{/if}
-	</p>
-<Aside corpusData={data.corpusData} currentName={item.name} />
+<h1 class="text-center p-3">{pageTitle}</h1>
+
+<div class="row">
+	<div class="col-md-2">
+		<Aside corpusData={data.corpusData} currentName={item.name} />
+	</div>
+	<div class="col-md-10"><p>{dummyText}</p></div>
+</div>
